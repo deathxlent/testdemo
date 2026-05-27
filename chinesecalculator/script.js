798ea1734b63d3cbd492f753aa07d4fb1c4e4a08
xlent <xlent@126.com>
2026-05-27 10:48:15 +0800

    master chinesecalculator 2

--- chinesecalculator/script.js
+++ chinesecalculator/script.js
@@ -20,6 +20,16 @@ function numberToChinese(num) {
     const isNegative = num < 0;
     num = Math.abs(num);
     
+    const numStr = num.toString();
+    
+    if (numStr.includes('e')) {
+        return (isNegative ? '负' : '') + numStr;
+    }
+    
+    if (num >= 1e16) {
+        return (isNegative ? '负' : '') + num.toExponential(6).replace(/\.?0+e/, 'e');
+    }
+    
     const integerPart = Math.floor(num);
     const decimalPart = num - integerPart;
     
@@ -57,8 +67,11 @@ function numberToChinese(num) {
     
     if (decimalPart > 0) {
         result += '点';
-        const decimalDigits = decimalPart.toFixed(10).replace(/0+$/, '').split('.')[1] || '';
-        for (const d of decimalDigits) {
+        let decimalStr = decimalPart.toFixed(10).replace(/\.?0+$/, '').split('.')[1] || '';
+        if (decimalStr.length > 10) {
+            decimalStr = decimalStr.substring(0, 10);
+        }
+        for (const d of decimalStr) {
             result += chineseNumbers[parseInt(d)];
         }
     }
@@ -69,20 +82,48 @@ function numberToChinese(num) {
 function expressionToChinese(expr) {
     if (!expr || expr === '请输入算式') return '';
     
-    let chineseExpr = expr;
+    let result = '';
+    let i = 0;
     
-    chineseExpr = chineseExpr.replace(/\*/g, ' 乘 ');
-    chineseExpr = chineseExpr.replace(/\+/g, ' 加 ');
-    chineseExpr = chineseExpr.replace(/-/g, ' 减 ');
-    chineseExpr = chineseExpr.replace(/\//g, ' 除以 ');
-    
-    const numbers = expr.match(/-?\d+\.?\d*/g) || [];
-    for (const num of numbers) {
-        const numFloat = parseFloat(num);
-        chineseExpr = chineseExpr.replace(num, numberToChinese(numFloat));
+    while (i < expr.length) {
+        const char = expr[i];
+        
+        if (char === '-' && (i === 0 || /[\+\-\*\/]/.test(expr[i - 1]))) {
+            let numStr = '-';
+            i++;
+            while (i < expr.length && (/[0-9.]/.test(expr[i]))) {
+                numStr += expr[i];
+                i++;
+            }
+            const num = parseFloat(numStr);
+            result += numberToChinese(num);
+        } else if (/[0-9.]/.test(char)) {
+            let numStr = '';
+            while (i < expr.length && (/[0-9.]/.test(expr[i]))) {
+                numStr += expr[i];
+                i++;
+            }
+            const num = parseFloat(numStr);
+            result += numberToChinese(num);
+        } else if (char === '+') {
+            result += ' 加 ';
+            i++;
+        } else if (char === '-') {
+            result += ' 减 ';
+            i++;
+        } else if (char === '*') {
+            result += ' 乘 ';
+            i++;
+        } else if (char === '/') {
+            result += ' 除以 ';
+            i++;
+        } else {
+            result += char;
+            i++;
+        }
     }
     
-    return chineseExpr;
+    return result;
 }
 
 function updateDisplay() {
@@ -154,9 +195,31 @@ function calculatePreview() {
 
 function formatResult(num) {
     if (Number.isInteger(num)) {
+        if (Math.abs(num) >= 1e16) {
+            return num.toExponential(6).replace(/\.?0+e/, 'e');
+        }
         return num.toString();
     }
-    const str = num.toFixed(10).replace(/\.?0+$/, '');
+    
+    const absNum = Math.abs(num);
+    
+    if (absNum >= 1e16) {
+        return num.toExponential(6).replace(/\.?0+e/, 'e');
+    }
+    
+    if (absNum < 1e-10 && absNum > 0) {
+        return num.toExponential(6).replace(/\.?0+e/, 'e');
+    }
+    
+    let str = num.toFixed(12);
+    str = str.replace(/\.?0+$/, '');
+    
+    const parts = str.split('.');
+    if (parts[1] && parts[1].length > 10) {
+        parts[1] = parts[1].substring(0, 10);
+        str = parts.join('.');
+    }
+    
     return str;
 }
 
@@ -172,12 +235,25 @@ function calculate() {
             expr = expr.slice(0, -1);
         }
         
+        const tokens = expr.match(/(\d+\.?\d*|[\+\-\*\/])/g) || [];
+        let isDivideByZero = false;
+        for (let i = 0; i < tokens.length; i++) {
+            if (tokens[i] === '/' && parseFloat(tokens[i + 1]) === 0) {
+                isDivideByZero = true;
+                break;
+            }
+        }
+        
         const result = eval(expr);
         
         if (isNaN(result)) {
             currentResult = '无效运算';
         } else if (!isFinite(result)) {
-            currentResult = '数值过大';
+            if (isDivideByZero) {
+                currentResult = '除数不能为零';
+            } else {
+                currentResult = '数值过大';
+            }
         } else {
             currentResult = formatResult(result);
             currentExpression = currentResult;
